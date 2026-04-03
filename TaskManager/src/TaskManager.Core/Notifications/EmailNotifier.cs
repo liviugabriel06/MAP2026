439e41fefba7174af863fa7c6000730f5d601939
Iriminescu Liviu <liviuiriminescu146@gmail.com>
2026-04-03 15:52:59 +0300

    commit v2

--- TaskManager/src/TaskManager.Core/Notifications/EmailNotifier.cs
+++ TaskManager/src/TaskManager.Core/Notifications/EmailNotifier.cs
@@ -1,4 +1,5 @@
 using System;
+using MimeKit;
 using TaskManager.Core.Interfaces;
 using TaskManager.Core.Models;
 
@@ -6,8 +7,37 @@ namespace TaskManager.Core.Notifications;
 
 public class EmailNotifier : ITaskNotifier
 {
+
+    private readonly string _emailAddress = "Trece_Emailul_Aici@gmail.com";
+    private readonly string _appPassword = "Trece_Parola_Aici";
     public void Notify(TaskItem task)
     {
-        Console.WriteLine($"[EMAIL NOTIFIER] Trimitere email catre echipa: Sarcina '{task.Title}' e gata.");
+        try
+        {
+            var message = new MimeMessage();
+
+            message.From.Add(new MailboxAddress("Task Manager App", _emailAddress));
+            message.To.Add(new MailboxAddress("Mine", _emailAddress));
+
+            message.Subject = $"Task finalizat: {task.Title}";
+
+            message.Body = new TextPart("plain")
+            {
+                Text = $"Salut \n Te informez ca task-ul '{task.Title}' a fost finalizat. \n Detalii: \n Descriere: {task.Description} \n Prioritate: {task.Priority} \n\n Zi frumoasa!"
+            };
+
+            using (var client = new MailKit.Net.Smtp.SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 465, true);
+
+                client.Authenticate(_emailAddress, _appPassword);
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"[EmailNotifier] Eroare la trimiterea email-ului: {ex.Message}");
+        }
     }
 }
