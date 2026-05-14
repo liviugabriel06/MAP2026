e1d76721c9e48454ae55dada59cb092c27729d97
Iriminescu Liviu <liviuiriminescu146@gmail.com>
2026-05-14 17:14:08 +0300

    Commit

--- MusicPlayer/MusicPlayer/obj/Debug/net8.0-windows/MusicPlayer.AssemblyInfo.cs
+++ MusicPlayer/MusicPlayer/obj/Debug/net8.0-windows/MusicPlayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicPlayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+732de2884632c85b645fdeb092c25637c98a5073")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+554ac6d6c4e14ce230a0e67a1c217aef1c85cb52")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicPlayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicPlayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MusicPlayer/MusicPlayer/obj/Debug/net8.0-windows/Views/MainWindow.g.i.cs
+++ MusicPlayer/MusicPlayer/obj/Debug/net8.0-windows/Views/MainWindow.g.i.cs
@@ -88,7 +88,7 @@ namespace MusicPlayer.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MusicPlayer;component/views/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MusicPlayer;V1.0.0.0;component/views/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
